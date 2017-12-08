7b41e9e0eb528b6227a0d8777a31899b80a1e5d3
Chapman Nitta <chapman.nitta@gmail.com>
2017-12-08 17:11:39 -0500

    Updated Node Modules

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var http, director, cool, bot, router, server, port;
+var http, director, cool, bot, router, server, port, r;
 
 http        = require('http');
 director    = require('director');
 cool        = require('cool-ascii-faces');
 bot         = require('./bot.js');
+r           = require('request')
 
 router = new director.http.Router({
   '/' : {
